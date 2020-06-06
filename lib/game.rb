8474c45cf46b3801d753507c97bc2e5c9bb8f085
PARDEEP KAUR <pkaur84@aol.com>
2020-06-06 22:46:57 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,94 +1,76 @@
-require "pry"
-
 class Game
-  attr_accessor :board, :player_1, :player_2, :timer
+
+  attr_accessor :board, :player_1, :player_2, :winner, :user_input
 
   WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-  ]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6] ]
 
-  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new, wargame = false)
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
-    @wargame = wargame
-    @timer = 1.5
+    @board.display
   end
 
   def current_player
-    board.turn_count.even?  ? player_1 : player_2
+     board.turn_count.odd? ? player_2 : player_1
   end
+
   def won?
-    WIN_COMBINATIONS.find do |combo|
-      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != " "
-    end
-  end
-  def draw?
-    board.full? && !won?
-  end
-  def over?
-    won? || draw?
-  end
-  def winner
-    board.cells[won?[0]] if won?
-  end
-  def turn
-    puts "It's now #{current_player.token}'s turn."
-    input = current_player.move(board, timer).to_i
-    if board.valid_move?(input.to_s)
-      board.update(input, current_player)
-      system('clear')
-      puts "Game #{@counter}" if @wargame
-      board.display
-    elsif input.between?(1, 9) == false
-      puts "That is an invalid move"
-      turn
-    else
-      puts "Whoops! Looks like that position is taken"
-      turn
-    end
-  end
-  def play
-    board.reset!
-    system('clear')
-    puts "Game #{@counter}" if @wargame
-    board.display
-    until over?
-      turn
-    end
-    if draw?
-      puts "WINNER: NONE"
-    elsif won?
-      puts "WINNER: #{winner}"
-    end
-  end
-  def wargames
-    @counter = 0
-    x = 0
-    o = 0
-    draw = 0
-    until @counter == 100
-      @counter += 1
-      play
-      if draw?
-        draw += 1
-      elsif winner == "X"
-        x += 1
-      elsif winner == "O"
-        o += 1
+    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
+        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
+          @board.cells[combination[1]] == @board.cells[combination[2]] &&
+          @board.taken?(combination[0]+1)
+          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
+          return combination
+        end
       end
-      sleep(@timer*1.5)
-      @timer -= (@timer/3)
-    end
-    puts "This round had #{x} wins for X, #{o} wins for O, and #{draw} draws."
-    puts "A STRANGE GAME. THE ONLY WINNING MOVE IS NOT TO PLAY."
-    puts "HOW ABOUT A NICE GAME OF CHESS?"
+    return false
   end
+
+  def draw?
+    @board.full? && !won? ? true : false
+  end
+
+  def over?
+     (won? || draw?) ? true : false
+     # IF board is not full, game is in progress (FALSE), ELSE, game is over (TRUE)
+  end
+
+  def winner
+    if won?
+      combination = won?
+      @board.cells[combination[0]] # X or O
+    end
+  end
+
+  def turn
+    puts "Please enter a number 1-9:"
+    @user_input = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else puts "Please enter a number 1-9:"
+      @board.display
+      turn
+    end
+    @board.display
+  end
+
+  def play
+    turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+
 end
